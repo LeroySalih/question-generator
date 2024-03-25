66486bc655c09348da4c2c6d563088b47c18462e
Leroy Salih <leroy.salih@gmail.com>
2024-03-25 07:53:11 +0300

    Working on script

--- textbook.js
+++ textbook.js
@@ -1,17 +1,24 @@
+
+// todo: check questions answers must match available questions
+// todo: document the command parameters
+
 import dotenv from 'dotenv';
 dotenv.config();
-
 import OpenAI from 'openai';
 import * as fs from 'fs';
+import {readFile} from "fs/promises";
+
+
 import { YoutubeTranscript } from 'youtube-transcript';
 import { createClient } from '@supabase/supabase-js'
 
+
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 
-async function main(url, specId, specTag, code){
+async function processRecord(url, specId, specTag, code, title){
 
   try{
 
@@ -19,27 +26,62 @@ async function main(url, specId, specTag, code){
     const supabase = createClient(process.env.SUPABASE_PUBLIC_URL, process.env.SUPABASE_SERVICE_KEY)
 
     const specItemId = await getSpecItemIdFromTag(supabase, specId, specTag);
-    console.log("specItemId", specItemId);
+    
+    if (specItemId === undefined){
+      console.error(`Cant Find spec Item ID for '${specId}' '${specTag}'`)
+      return;
+    }
 
+    console.log("Found specItemId", specId, specTag, specItemId);
 
-   const transcript = await getTranscript(url);
+    const questionCount = await getQuestionCount (supabase, code) 
+
+    console.log(`Found ${questionCount} questions.`)
+    
+    if (questionCount >= 20){
+      console.log(`Too many questions (${questionCount}) for ${code}.  Skipping`);
+      return;
+    }
+
+   const {transcript, error} = await getTranscript(url);
+
+   if (error){
+    console.error("Error detected with code", code, ". exiting");
+    return;
+   }
+
+   
 
    console.log("## Transcript received.", transcript.length, "chars")
+   // console.log("## Transcript received.", transcript.slice(0,300), "chars")
    
 
    const summary = await getSummary(transcript);
 
    console.log("## Summary received.", summary.length, "chars")
+   // console.log("## Summary received.", summary.slice(0, 300), "chars")
 
-   const result = await uploadPage(supabase, code, specId, specItemId, transcript, summary)
+   const result = await uploadPage(supabase, code, specId, specItemId, transcript, summary, title)
 
    console.log("DB updated, result:", result);
 
-   const questions = await getQuestions(transcript);
-
-   const questionResult = await uploadQuestions(supabase, specItemId, code, questions)
-
+   let questions = await getQuestions(transcript);
+   let questionResult = await uploadQuestions(supabase, specItemId, code, questions)
    console.log("Uploaded ", questionResult.length, "questions")
+
+   questions = await getQuestions(transcript);
+   questionResult = await uploadQuestions(supabase, specItemId, code, questions)
+   console.log("Uploaded ", questionResult.length, "questions")
+
+   questions = await getQuestions(transcript);
+   questionResult = await uploadQuestions(supabase, specItemId, code, questions)
+   console.log("Uploaded ", questionResult.length, "questions")
+
+   questions = await getQuestions(transcript);
+   questionResult = await uploadQuestions(supabase, specItemId, code, questions)
+   console.log("Uploaded ", questionResult.length, "questions")
+
+   return true;
 
   } catch (error) {
     console.error(error);
@@ -47,10 +89,36 @@ async function main(url, specId, specTag, code){
   
 }
 
-async function getTranscript(url){
-  const response = await YoutubeTranscript.fetchTranscript(url);
+async function getQuestionCount(supabase, code) {
+  try {
+    const {count, error} = await supabase.from("dqQuestions")
+                  .select('*', { count: 'exact', head: true })
+                  .eq("code", code);
+    
+    if (error) {
+      throw new Error(error.message)
+    }
 
-  return  response.reduce((prev, curr) => prev + curr.text, "")
+    console.log("Count Data", count);
+    return count;
+
+  } catch(error){
+    console.error(error.message);
+    return null;
+  }
+}
+
+async function getTranscript(url){
+  try{
+
+    const response = await YoutubeTranscript.fetchTranscript(url);
+
+  return  {transcript: response.reduce((prev, curr) => prev + curr.text, ""), error: null}
+  } catch(error) {
+    console.error(error.message);
+    return {transcript: null, error}
+  }
+  
   
 }
 
@@ -110,6 +178,8 @@ async function getQuestions(transcript) {
 
 const args = JSON.parse(response.choices[0].message.function_call.arguments);
 
+console.log(args);
+
 return args;
 }
 
@@ -142,14 +212,15 @@ async function getSpecItemIdFromTag(supabase, specId, tag) {
 
   }catch(error){
     console.error(error);
+    
   }
 }
 
-async function uploadPage(supabase, code, specId, specItemId, transcript, summary) {
+async function uploadPage(supabase, code, specId, specItemId, transcript, summary, title) {
 
   try{
     
-    const {data, error} = await supabase.from("dqPage").upsert({id: code, summary, specItemId, transcript}).select("id")
+    const {data, error} = await supabase.from("dqPage").upsert({id: code, summary, specItemId, transcript, title}).select("id")
     
     if (error) {
       throw(error);
@@ -173,7 +244,7 @@ async function uploadQuestions(supabase, specItemId, code, questions) {
       const choices = questions.choices[i];
       const correct_answer = questions.correct_answers[i][0];
 
-      // console.log(question_text, choices, correct_answer)
+      // console.log(question_text)
       const {data, error} = await supabase.from("dqQuestions").insert({ specItemId, code, question_text, choices, correct_answer}).select("id")
     
       if (error) {
@@ -193,24 +264,88 @@ async function uploadQuestions(supabase, specItemId, code, questions) {
 }
 
 
-const code= process.argv[2];
-const specId = process.argv[3];
-const specItemId = process.argv[4]
 
-if(process.argv.length != 5){
-  console.error("Incorrect number of args")
-  process.exit();
+async function readThisFile(filePath) {
+  try {
+
+    const data = await readFile(filePath);
+    const lines = data.toString().split('\n')
+    const uploadData = [];
+
+    for (const line of lines){
+      
+      const fields = line.split(",")
+      
+      if (fields.length < 5) {
+        continue;
+      }
+
+      if (fields[3].length == 0) {
+        continue;
+      }
+
+
+      const [specId, specTag, specTagTitle, code, title] = [...fields];
+
+      uploadData.push({specId, specTag, code, title: title.trim()})
+    }
+
+    return uploadData
+
+    
+  } catch (error) {
+    console.error(`Got an error trying to read the file: {error.message}`);
+ }
 }
 
-console.log(`Getting data for `, code)
-console.log(`SpecId`, specId)
-console.log(`SpecItemId`, specItemId)
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-// node textbook.js 'mUxgOlnwoHo' 1 3.1.1;  
-//
-// 'mUxgOlnwoHo'  - url code
-// 1              - specId
-// 3.1.1          - SpecTag
-main(`https://youtu.be/mUxgOlnwoHo${code}`, specId, specItemId, code)
+
+
+
+const processFile = async (fileName) => {
+  
+  const uploadData = await readThisFile(`./${fileName}.csv`)
+
+  console.log("main", uploadData);
+  
+  for (const data of uploadData){
+    const result = await processRecord(`https://youtu.be/${data.code}`, data.specId, data.specTag, data.code, data.title)
+    
+    if (result == true){
+      console.log("Update worked, sleeping")
+      await sleep(10000);
+    }
+    
+  }
+
+}
+
+console.log("Processing argvs", process.argv);
+
+// process a file
+if (process.argv[2].slice(0, 2) == "-f"){
+  const fileName = process.argv[2].split(":")[1];
+  console.log("Processing", fileName)
+  await processFile(fileName);
+}
+
+if (process.argv[2].slice(0, 2) == "-c"){
+  const code = process.argv[3];
+  const specId = process.argv[4];
+  const tag = process.argv[5];
+  const title = process.argv[6];
+
+  console.log("Processing", code, specId, tag, title);
+  await processRecord(`https://youtu.be/${code}`, specId, tag, code, title);
+}
+
+process.exit(0);
+
+
 
 
